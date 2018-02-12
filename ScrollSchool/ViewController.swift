e7c179ab0b43a724cfcc2fdaaef743cb128e1ccf
pushpendra.khandelwal@kuliza.com <pushpendra.khandelwal@kuliza.com>
2018-02-12 11:19:44 +0530

    project setup

---
+++ ScrollSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollSchool
+//
+//  Created by Pushpendra Khandelwal on 12/02/18.
+//  Copyright © 2018 Pushpendra Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
